f66a8396c5f60cea3ced3943a3e567c0256feec7
M-Arcones <43450871+M-Arcones@users.noreply.github.com>
2020-10-17 22:47:11 +0200

    nueva actividad y boton para acceder

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.Btn_jugar).setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this,Pregunta_RadioButtons.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Pregunta_RadioButtons.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Pregunta_RadioButtons extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta__radio_buttons);
+    }
+}
