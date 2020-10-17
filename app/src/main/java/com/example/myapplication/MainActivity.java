35834af15437e4fdc8f3947a7d80d75093a71523
M-Arcones <43450871+M-Arcones@users.noreply.github.com>
2020-10-18 01:29:27 +0200

    leer preguntas de xml String + crear array de preguntas ordenadas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
@@ -16,7 +20,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.Btn_jugar).setOnClickListener(this);
-    }
+
+        int n_preg=Integer.parseInt(getResources().getString(this.getResources().getIdentifier("num_preguntas", "string", this.getPackageName())));
+        ArrayList<String[]> Preguntas = new ArrayList<String[]>();
+        int n_preguntas_totales=3;
+        Random rnd = new Random();
+        rnd.setSeed(6);//cambiar a la hora
+
+        List<Integer> PosicionesDisponibles = new ArrayList<Integer>();
+        for(int i=0;i<n_preguntas_totales;i++){
+            PosicionesDisponibles.add(i);
+        }
+
+        //int randomNum = rnd.nextInt(99999);
+        //((TextView)findViewById(R.id.txtVistaPrueba)).setText(""+randomNum);
+
+        for(int i=0;i<n_preg;i++){
+            int randomNum = rnd.nextInt((PosicionesDisponibles.size()));
+            String nombre_preg="pregunta"+PosicionesDisponibles.get(randomNum);
+            PosicionesDisponibles.remove(randomNum);
+            //int idPreg = getResources().getStringArray(this.getResources().getIdentifier("pregunta0", "array", this.getPackageName()));
+            //((TextView)findViewById(R.id.txtVistaPrueba)).setText(n_preg);
+            Preguntas.add(getResources().getStringArray(this.getResources().getIdentifier(nombre_preg, "array", this.getPackageName())));
+        }
+        ((TextView)findViewById(R.id.txtVistaPrueba)).setText(Preguntas.get(0)[0]);
+        ((TextView)findViewById(R.id.txtVistaPrueba2)).setText(Preguntas.get(1)[0]);
+        ((TextView)findViewById(R.id.txtVistaPrueba3)).setText(Preguntas.get(2)[0]);
+}
 
     public void onClick(View v) {
         Intent intent = new Intent(this,Pregunta_RadioButtons.class);
