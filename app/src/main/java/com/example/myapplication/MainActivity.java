0768396559cbe0c9cf3a9a4e49e7f0ca417df4aa
M-Arcones <43450871+M-Arcones@users.noreply.github.com>
2020-10-19 00:19:23 +0200

    Quiz en una unica actividad

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,43 +26,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ArrayList<String[]> Preguntas = new ArrayList<String[]>();
 
         //int n_preg=Integer.parseInt(getResources().getString(this.getResources().getIdentifier("num_preguntas", "string", this.getPackageName())));
-
         int n_preguntas_totales=3;
-        Random rnd = new Random();
-        rnd.setSeed(6);//cambiar a la hora
-
-        /*Crea una lista de las posiciones disponibles para ordenar de manera aletoria las preguntas*/
-        List<Integer> PosicionesDisponibles = new ArrayList<Integer>();
-        for(int i=0;i<n_preguntas_totales;i++){
-            PosicionesDisponibles.add(i);
-        }
-
-        /*Añade en la lista de preguntas las preguntas leidas de string.xml cogidas de manera aleatoria por su "name" */
-        for(int i=0;i<n_preguntas_totales;i++){
-            int randomNum = rnd.nextInt((PosicionesDisponibles.size()));
-            String nombre_preg="pregunta"+PosicionesDisponibles.get(randomNum);
-            PosicionesDisponibles.remove(randomNum);
-            Preguntas.add(getResources().getStringArray(this.getResources().getIdentifier(nombre_preg, "array", this.getPackageName())));
-        }
-
-        /*Pasa a la actividad la lista Preguntas y la puntuacion*/
         Intent intent;
-        switch (Preguntas.get(0)[0]) {
-            case "Button":
-                intent = new Intent(this,Pregunta_RadioButtons.class);
-                break;
-            case "Seekbar":
-                intent = new Intent(this,Pregunta_Seekbar.class);
-                break;
-            case "Imagen":
-                intent = new Intent(this,Pregunta_Imagen.class);
-                break;
-            default:
-                intent = new Intent(this,Pregunta_RadioButtons.class);
-                break;
-        }
-        intent.putExtra("Preguntas", Preguntas);
-        intent.putExtra("Puntuacion", 0);
+        intent = new Intent(this,Pregunta_RadioButtons.class);
+        intent.putExtra("n_preguntas_totales", n_preguntas_totales);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/Pregunta_RadioButtons.java
+++ app/src/main/java/com/example/myapplication/Pregunta_RadioButtons.java
@@ -14,17 +14,46 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Pregunta_RadioButtons extends AppCompatActivity {
-
+public class Pregunta_RadioButtons extends AppCompatActivity implements View.OnClickListener{
+    ArrayList<String[]> Preguntas= new ArrayList<String[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregunta__radio_buttons);
 
-        ArrayList<String[]> Preguntas = (ArrayList<String[]>) getIntent().getSerializableExtra("Preguntas");
-        int Puntuacion = (int) getIntent().getSerializableExtra("Puntuacion");
-        ((TextView)findViewById(R.id.textView)).setText(""+ Puntuacion);
+        int n_preguntas_totales = (int) getIntent().getSerializableExtra("n_preguntas_totales");
 
+        Random rnd = new Random();
+        rnd.setSeed(6);//cambiar a la hora
+
+        /*Crea una lista de las posiciones disponibles para ordenar de manera aletoria las preguntas*/
+        List<Integer> PosicionesDisponibles = new ArrayList<Integer>();
+        for(int i=0;i<n_preguntas_totales;i++){
+            PosicionesDisponibles.add(i);
+        }
+
+        /*Añade en la lista de preguntas las preguntas leidas de string.xml cogidas de manera aleatoria por su "name" */
+        for(int i=0;i<n_preguntas_totales;i++){
+            int randomNum = rnd.nextInt((PosicionesDisponibles.size()));
+            String nombre_preg="pregunta"+PosicionesDisponibles.get(randomNum);
+            PosicionesDisponibles.remove(randomNum);
+            Preguntas.add(getResources().getStringArray(this.getResources().getIdentifier(nombre_preg, "array", this.getPackageName())));
+        }
+
+        /**/
+        ((TextView) findViewById(R.id.TxtPregunta)).setText(Preguntas.get(0)[1]);
+        Preguntas.remove(0);
+
+        /*Borrar Codigo pruebas*/
+        findViewById(R.id.BtnSiguientePregunta).setOnClickListener((View.OnClickListener) this);
     }
+
+    public void onClick(View v) {
+        if(Preguntas.size()>0){
+            ((TextView) findViewById(R.id.TxtPregunta)).setText(Preguntas.get(0)[1]);
+            Preguntas.remove(0);
+        }
+    }
+
 }
